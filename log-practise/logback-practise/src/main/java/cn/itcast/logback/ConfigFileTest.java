94be5a29b4cc138e92b7ac4a888d4bc9bbc7aea8
韩群 <>
2021-09-07 19:56:22 +0800

    logback学习完成

--- log-practise/logback-practise/src/main/java/cn/itcast/logback/ConfigFileTest.java
+++ log-practise/logback-practise/src/main/java/cn/itcast/logback/ConfigFileTest.java
@@ -25,6 +25,51 @@ public class ConfigFileTest {
             logger.warn("warn信息");
             logger.error("error信息");
         }
+    }
 
+    @Test
+    public void configFileTest3(){
+        Logger logger = LoggerFactory.getLogger(LogbackPrimerTest.class);
+        logger.trace("trace信息");
+        logger.debug("debug信息");
+        logger.info("info信息");
+        logger.warn("warn信息");
+        logger.error("error信息");
+    }
+
+    @Test
+    public void configFileTest4(){
+        Logger logger = LoggerFactory.getLogger(LogbackPrimerTest.class);
+        for (int i = 0; i < 100; i++) {
+            logger.trace("trace信息");
+            logger.debug("debug信息");
+            logger.info("info信息");
+            logger.warn("warn信息");
+            logger.error("error信息");
+        }
+        System.out.println("1-------------------");
+        System.out.println("2-------------------");
+        System.out.println("3-------------------");
+        System.out.println("4-------------------");
+        System.out.println("5-------------------");
+        System.out.println("6-------------------");
+    }
+
+    @Test
+    public void configFileTest5(){
+        Logger logger = LoggerFactory.getLogger(LogbackPrimerTest.class);
+        for (int i = 0; i < 100; i++) {
+            logger.trace("trace信息");
+            logger.debug("debug信息");
+            logger.info("info信息");
+            logger.warn("warn信息");
+            logger.error("error信息");
+        }
+        System.out.println("1-------------------");
+        System.out.println("2-------------------");
+        System.out.println("3-------------------");
+        System.out.println("4-------------------");
+        System.out.println("5-------------------");
+        System.out.println("6-------------------");
     }
 }
