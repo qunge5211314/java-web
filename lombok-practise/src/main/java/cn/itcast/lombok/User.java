96af291e4fe8af10d7c6fae9fe4b00f4d1ba6e96
JustinHan <>
2021-09-09 14:29:49 +0800

    lombok学习完成

--- lombok-practise/src/main/java/cn/itcast/lombok/User.java
+++ lombok-practise/src/main/java/cn/itcast/lombok/User.java
@@ -1,24 +1,34 @@
 package cn.itcast.lombok;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Command + 7查看类结构
  */
 
+
+//@AllArgsConstructor
+//@NoArgsConstructor
+//@ToString(exclude = {"age", "email"})
+//@EqualsAndHashCode(of = {"id", "password"})
+@Builder
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class User {
+//    static String s = "";
+//    final int i = 0;
     private Integer id;
     private String name;
+    //    @Getter(AccessLevel.PRIVATE)
+    //    @NonNull
     private Integer age;
+    //    @Setter
     private String email;
     private String password;
 
-    public void setId(Integer id){
+    public void setId(Integer id) {
         System.out.println("test");
         this.id = id;
 
--- lombok-practise/src/test/java/LombokTest.java
+++ lombok-practise/src/test/java/LombokTest.java
@@ -4,8 +4,21 @@ import org.junit.Test;
 public class LombokTest {
     @Test
     public void lombokTest1(){
-        User user = new User(1, "zhangsan", 18, "@qq.com", "123456");
-        user.setId(2);
+        User user = new User();
+        user.setId(1);
         System.out.println(user.getId());
+        System.out.println(user.toString());
+    }
+
+    @Test
+    public void lombokTest2(){
+        User user = User.builder().id(1).name("666").password("123456").age(13).email("@666.com").build();
+        System.out.println(user.toString());
+    }
+
+    @Test
+    public void lombokTest3(){
+        User user = User.builder().id(1).name("666").password("123456").age(13).email("@666.com").build();
+        System.out.println(user.toString());
     }
 }
